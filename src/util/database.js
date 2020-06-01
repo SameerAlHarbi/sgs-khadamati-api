18588256aed64ce41c327a66c2085ee9ebcdcc47
Sameer Al Harbi <smrhrbi@gmail.com>
2020-06-01 08:21:46 +0300

    db

--- src/util/database.js
+++ src/util/database.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('khadamati-db', 
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PASSWORD,
     { dialect: 'mssql', host: process.env.DB_SERVER, dialectOptions: {
